a21076b7f6b60767185c0f0dcf1bf3db43b298fa
stenligeorgiev <stanislav.georgiev11@gmail.com>
2016-03-10 17:52:14 +0200

    10

---
+++ number/number/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter  the number :");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("zero ");
+                    break;
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two ");
+                    break;
+                case 3:
+                    Console.WriteLine(" trree");
+                    break;
+                case 4:
+                    Console.WriteLine("four ");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine(" seven");
+                    break;
+                case 8:
+                    Console.WriteLine(" eight");
+                    break;
+                case 9:
+                    Console.WriteLine(" nine");
+                    break;
+            }
+        }
+    }
+}                  
+            
+            
+        
+    
+
