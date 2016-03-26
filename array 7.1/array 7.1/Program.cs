bb3d32837d9c8c62a5c3a9080718083509bd099d
stenligeorgiev <stanislav.georgiev11@gmail.com>
2016-03-26 19:27:27 +0200

    10

---
+++ array 7.1/array 7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter the number a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter  the  number b:");
+            int b = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[a, b];
+
+
+
+
+
+
+
+
+
+        }   
+    }
+}
---
+++ metod2/metod2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 7;
+            int c = 4;
+
+            int razlica = CalculateRaz(a, b, c);
+            Console.WriteLine(razlica);
+        }
+
+        private static int CalculateRaz(int a, int b, int c)
+        {
+            return a - b - c;
+        }
+    }
+}
